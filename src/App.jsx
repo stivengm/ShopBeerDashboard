057123a2f97783254c15415fec2ecec23c437eea
Stiven Gomez <stivengomez@soyyo.co>
2023-11-30 09:44:46 -0500

    FIX - Fix routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import { Login } from './components/user/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Dashboard />
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
