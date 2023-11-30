7e59b6366f7f986d2afbb380dee2a704faad5030
Stiven Gomez <stivengomez@soyyo.co>
2023-11-30 11:49:26 -0500

    FIX - Ajuste de menu.

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,11 +1,14 @@
 import { Header } from '../shared/header/header';
+import { Menu } from '../widgets/menu/menu';
 
 export function Dashboard() {
     return(
         <>
             <Header />
 
+
             <div className='content'>
+                <Menu />
                 Hola Dashboard
             </div>
         </>
---
+++ src/components/widgets/menu/menu.jsx
@@ -0,0 +1,11 @@
+import './menu.css';
+
+export function Menu() {
+    return(
+        <>
+            <div className='menu'>
+                Hola Menu
+            </div>
+        </>
+    )
+}
