bc88a4509de53f8f9c427825103eeaf7b725d182
Stiven Gomez <stivengomez@soyyo.co>
2023-11-30 09:34:17 -0500

    ADD - Add dashbord.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Login } from './components/user/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
 import './App.css'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-
+      <Dashboard />
       <Login />
     </>
   )
---
+++ src/components/dashboard/dashboard.jsx
@@ -0,0 +1,8 @@
+
+export function Dashboard() {
+    return(
+        <div>
+            Hola Dashboard
+        </div>
+    )
+}
